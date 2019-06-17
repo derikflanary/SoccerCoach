e0fd9ecf73108b89d3c1929bd6c3d7b6ebda6fa7
Derik Flanary <derikflanary@gmail.com>
2019-06-17 09:19:40 -0600

    Initial Commit

---
+++ soccer-coach/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  soccer-coach
+//
+//  Created by Derik Flanary on 6/17/19.
+//  Copyright © 2019 Derik Flanary. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
